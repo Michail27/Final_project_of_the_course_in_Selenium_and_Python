ca854cda35e576b8e532c6fdb6ef3e18c203e5ef
Michail27 <michail27.03@mail.ru>
2020-09-23 08:29:02 +0300

    Created Class LoginPage and created Class LoginPageLocators

--- test_main_page.py
+++ test_main_page.py
@@ -1,9 +1,10 @@
 from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
 
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
+    page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
     page.open()                      # открываем страницу
     page.go_to_login_page()          # выполняем метод страницы - переходим на страницу логина
 
@@ -13,3 +14,10 @@ def test_guest_should_see_login_link(browser):
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
+
+
+def test_guest_should_see_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page2 = LoginPage(browser, link)
+    page2.open()
+    page2.should_be_login_page()
